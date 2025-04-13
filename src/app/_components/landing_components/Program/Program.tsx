585b89c0b900a088e9f0be2779a5fecdc2060d8d
lgwedding <lgpap97@gmail.com>
2025-04-13 20:47:16 +0200

    Program details added

---
+++ src/app/_components/landing_components/Program/Program.tsx
@@ -0,0 +1,333 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import styles from "./program.module.css";
+import { Menu, MenuItem, Paper, Typography } from "@mui/material";
+
+const Program = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [activeMenu, setActiveMenu] = useState("");
+
+  const handleClick = (event: any, menuId: string) => {
+    setAnchorEl(event.currentTarget);
+    setActiveMenu(menuId);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setActiveMenu("");
+  };
+  return (
+    <div className={styles.program_container}>
+      <h2 className={styles.section_title}>Program</h2>
+      <div className={styles.program_inner_container}>
+        <div className={styles.program}>
+          <div
+            className={styles.program_icon_church}
+            onClick={(e) => handleClick(e, "curch")}
+            style={{ cursor: "pointer" }}
+          >
+            <Image
+              src="/church.jpg"
+              alt="curch icon"
+              className={styles.church_image}
+              width={100}
+              height={100}
+            />
+            <p>15:00</p>
+            <p>Templomi szertartás</p>
+          </div>
+          <Menu
+            anchorEl={anchorEl}
+            open={activeMenu === "curch"}
+            onClose={handleClose}
+            disableScrollLock
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            <div style={{ padding: "1rem", maxWidth: 300 }}>
+              <Typography variant="h6" gutterBottom>
+                Templomi szertartás részletek
+              </Typography>
+              <Typography variant="body2">Időpont: 15:00</Typography>
+              <Typography variant="body2">
+                Helyszín: Szilvásváradi Református Kerektemplom
+              </Typography>
+              <Typography variant="body2">Időtartam: fél óra</Typography>
+            </div>
+          </Menu>
+          <div
+            className={styles.program_icon_rings}
+            onClick={(e) => handleClick(e, "rings")}
+            style={{ cursor: "pointer" }}
+          >
+            <Image
+              src="/rings.jpg"
+              alt="rings icon"
+              className={styles.rings_image}
+              width={100}
+              height={100}
+            />
+            <p>17:30</p>
+            <p>Polgári ceremónia</p>
+          </div>
+          <Menu
+            anchorEl={anchorEl}
+            open={activeMenu === "rings"}
+            onClose={handleClose}
+            disableScrollLock
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            <div style={{ padding: "1rem", maxWidth: 300 }}>
+              <Typography variant="h6" gutterBottom>
+                Polgári ceremónia részletek
+              </Typography>
+              <Typography variant="body2">Időpont: 17:30</Typography>
+              <Typography variant="body2">
+                Helyszín: Boróka Tábor kertje
+              </Typography>
+              <Typography variant="body2">Időtartam: fél óra</Typography>
+            </div>
+          </Menu>
+          <div
+            className={styles.program_icon_cake}
+            onClick={(e) => handleClick(e, "cake")}
+            style={{ cursor: "pointer" }}
+          >
+            <Image
+              src="/cake.jpg"
+              alt="cake icon"
+              className={styles.cake_image}
+              width={100}
+              height={100}
+            />
+            <p>22:00</p>
+            <p>Esküvői Torta</p>
+          </div>
+          <Menu
+            anchorEl={anchorEl}
+            open={activeMenu === "cake"}
+            onClose={handleClose}
+            disableScrollLock
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            <div style={{ padding: "1rem", maxWidth: 300 }}>
+              <Typography variant="h6" gutterBottom>
+                Esküvői Torta részletek
+              </Typography>
+              <Typography variant="body2">Időpont: 22:00</Typography>
+              <Typography variant="body2">Helyszín: Boróka Tábor</Typography>
+              <Typography variant="body2">Időtartam: Kitudja</Typography>
+              <Typography variant="body2">
+                Torta ízesítés: pisztácia tuti
+              </Typography>
+            </div>
+          </Menu>
+          <div
+            className={styles.program_icon_plate}
+            onClick={(e) => handleClick(e, "plate")}
+            style={{ cursor: "pointer" }}
+          >
+            <Image
+              src="/plate.jpg"
+              alt="plate icon"
+              className={styles.plate_image}
+              width={100}
+              height={100}
+            />
+            <p>0:00</p>
+            <p>Éjféli falatozás</p>
+          </div>
+          <Menu
+            anchorEl={anchorEl}
+            open={activeMenu === "plate"}
+            onClose={handleClose}
+            disableScrollLock
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            <div style={{ padding: "1rem", maxWidth: 300 }}>
+              <Typography variant="h6" gutterBottom>
+                Éjféli falatozás részletek
+              </Typography>
+              <Typography variant="body2">Időpont: 0:00</Typography>
+              <Typography variant="body2">Helyszín: Boróka Tábor</Typography>
+              <Typography variant="body2">Időtartam: Valameddig</Typography>
+              <Typography variant="body2">Menü: Töltött káposzta</Typography>
+            </div>
+          </Menu>
+          <Image
+            src="/footsteps.png"
+            alt="Program schedule"
+            width={250}
+            height={500}
+            className={styles.footsteps_image}
+          />
+          <div
+            className={styles.program_icon_glasses}
+            onClick={(e) => handleClick(e, "glasses")}
+            style={{ cursor: "pointer" }}
+          >
+            <Image
+              src="/glasses.jpg"
+              alt="glasses icon"
+              className={styles.glasses_image}
+              width={100}
+              height={100}
+            />
+            <p>16:00</p>
+            <p>Vendégfogadás</p>
+          </div>
+          <Menu
+            anchorEl={anchorEl}
+            open={activeMenu === "glasses"}
+            onClose={handleClose}
+            disableScrollLock
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            <div style={{ padding: "1rem", maxWidth: 300 }}>
+              <Typography variant="h6" gutterBottom>
+                Vendégfogadás részletek
+              </Typography>
+              <Typography variant="body2">Időpont: 16:00</Typography>
+              <Typography variant="body2">
+                Helyszín: Boróka Tábor kertje
+              </Typography>
+              <Typography variant="body2">
+                Időtartam: kb. 1 és fél óra
+              </Typography>
+              <Typography variant="body2">
+                Vendégváró falatok hamarosan...
+              </Typography>
+            </div>
+          </Menu>
+          <div
+            className={styles.program_icon_camera}
+            onClick={(e) => handleClick(e, "camera")}
+            style={{ cursor: "pointer" }}
+          >
+            <Image
+              src="/camera.jpg"
+              alt="camera icon"
+              className={styles.camera_image}
+              width={100}
+              height={100}
+            />
+            <p>18:00</p>
+            <p>Fotózás</p>
+          </div>
+
+          <Menu
+            anchorEl={anchorEl}
+            open={activeMenu === "camera"}
+            onClose={handleClose}
+            disableScrollLock
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            <div style={{ padding: "1rem", maxWidth: 300 }}>
+              <Typography variant="h6" gutterBottom>
+                Fotózás részletek
+              </Typography>
+              <Typography variant="body2">Időpont: 18:00</Typography>
+              <Typography variant="body2">
+                Helyszín: Boróka Tábor kertje
+              </Typography>
+              <Typography variant="body2">Időtartam: kb. 1 óra</Typography>
+            </div>
+          </Menu>
+          <div
+            className={styles.program_icon_spoonandfork}
+            onClick={(e) => handleClick(e, "spoonandfork")}
+            style={{ cursor: "pointer" }}
+          >
+            <Image
+              src="/spoonandfork.jpg"
+              alt="spoonandfork icon"
+              className={styles.spoonandfork_image}
+              width={100}
+              height={100}
+            />
+            <p>19:30</p>
+            <p>Ünnepi vacsora</p>
+          </div>
+          <Menu
+            anchorEl={anchorEl}
+            open={activeMenu === "spoonandfork"}
+            onClose={handleClose}
+            disableScrollLock
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            <div style={{ padding: "1rem", maxWidth: 300 }}>
+              <Typography variant="h6" gutterBottom>
+                Ünnepi vacsora részletek
+              </Typography>
+              <Typography variant="body2">Időpont: 19:30</Typography>
+              <Typography variant="body2">Helyszín: Boróka Tábor</Typography>
+              <Typography variant="body2">Időtartam: kb. 1 óra</Typography>
+              <Typography variant="body2">Menü: Hamarosan...</Typography>
+            </div>
+          </Menu>
+        </div>
+        {/* 
+      <div className={styles.program_card}>
+        <p className={styles.program_text}>15:00: Templomi szertartás</p>
+        <p className={styles.program_text}>16:00: Vendégfogadás</p>
+        <p className={styles.program_text}>17:30: Polgári ceremónia</p>
+        <p className={styles.program_text}>18:00: Fotózás</p>
+        <p className={styles.program_text}>19:30: Ünnepi vacsora</p>
+        <p className={styles.program_text}>22:00: Esküvői torta</p>
+        <p className={styles.program_text}>00:00: Éjféli falatozás</p>
+      </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Program;
