00aa5e08b30f8892f330dadaf376f4bc0246df83
Pap Gergő <pap.gergo.1997@gmail.com>
2025-05-10 11:10:41 +0200

    Program schedule updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off', // 👈 disables the 'any' rule
+    },
+  })
 ];
+
 
 export default eslintConfig;
